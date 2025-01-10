6c98412eafb8ac01fc87608481ab8ee0ea86498a
Danil Galeev <galoffdan@gmail.com>
2025-01-10 18:26:31 +0300

    Add get specific user tasks api

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,7 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.models import Task, User
 from api.serializers import TaskModelSerializer, UserModelSerializer
@@ -7,6 +10,11 @@ from api.serializers import TaskModelSerializer, UserModelSerializer
 class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+    @swagger_auto_schema(method="get", responses={200: TaskModelSerializer(many=True)})
+    @action(detail=True, methods=["get"])
+    def tasks(self, request, *args, **kwargs):
+        return Response(TaskModelSerializer(self.get_object().tasks.all(), many=True).data)
 
 
 class TaskViewSet(
